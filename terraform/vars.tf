f4297e24da74161cc153cea0b853c02fefad4d04
denisseroka9 <denisserokavalido@gmail.com>
2026-03-09 20:12:53 +0000

    initial terraform setup

---
+++ terraform/vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region"
+  default     = "westeurope"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  default     = "rg-cp2-devops"
+}
